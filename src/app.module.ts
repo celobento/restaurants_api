a15585284d21df4ee2e0154730f7bdd0da533a6b
Marcelo Bento <celobento26@gmail.com>
2024-08-20 22:01:45 -0300

    setting mongdb access

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule} from "@nestjs/mongoose"
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }), 
+    MongooseModule.forRoot(process.env.DB_URI_LOCAL)],
   controllers: [AppController],
   providers: [AppService],
 })
