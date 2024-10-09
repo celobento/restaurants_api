fe0a77de616e4a27c435b0ff9db9b9a146bcdbb5
Marcelo Bento <celobento26@gmail.com>
2024-10-08 21:13:43 -0300

    Testing RestaurantsService - create

--- src/restaurants/restaurants.service.spec.ts
+++ src/restaurants/restaurants.service.spec.ts
@@ -3,6 +3,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { Model } from 'mongoose';
 import { RestaurantsService } from '../restaurants/restaurants.service';
 import { Restaurant } from './schemas/restaurant.schema';
+import { UserRoles } from '../auth/schemas/use.schema';
 
 const mockRestaurant = {
   _id: '66fb2efc7c1a9531745002f4',
@@ -20,8 +21,16 @@ const mockRestaurant = {
   __v: 1,
 };
 
+const mockUser = {
+  name:"Marcelo",
+  email:"celo4@gmail.com",
+  _id:"6705bd14f674b1f18b9a06de", 
+  role: UserRoles.USER
+}
+
 const mockRestaurantService = {
   find: jest.fn(),
+  create: jest.fn()
 };
 
 describe('RestaurantService', () => {
@@ -58,6 +67,24 @@ describe('RestaurantService', () => {
       );
       const restaurants = await service.findAll({ keyword: 'restaurant' });
       expect(restaurants).toEqual([mockRestaurant]);
+    });
+  });
+
+  describe('create', () => {
+    const newRestaurant = {
+      name: 'Retaurant 8',
+      description: 'This is just a 8 description',
+      email: 'ghulam8@gamil.com',
+      phoneNumber: '+1(646)286-7644',
+      address: '200 Olympic Dr, Stafford, VS, 22554',
+      category: 'Fast Food',
+    }
+    it('should create a new restaurants', async () => {
+      // jest.spyOn(APIFeatures, 'getRestaurantLocation').mockImplementation(() => Promise.resolve(mockRestaurant.location));
+      // I can't do this test work, promise is not recognized
+      //jest.spyOn(model, 'create').mockImplementationOnce(() => Promise.resolve(mockRestaurant));
+      const result = await service.create(newRestaurant as any, mockUser as any);
+      expect(result).toEqual(mockRestaurant);
     });
   });
 });
